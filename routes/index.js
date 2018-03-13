a9d514e6b4fd7294f106ef9ce05787085a8e8d8a
Russell Dutton <rdutton1504@gmail.com>
2018-03-13 20:26:34 +0200

    Created route 'generate' to generate hello world lua script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/generate', function(req, res, next) {
+  lua = "io.write(\"Hello World\\n\")";
+  fs.writeFile('./Lua/hello.lua', lua, (err) => {
+    if (err) throw err;
+    res.send("File Generated: hello.lua");
+  });
 });
 
 module.exports = router;
