5ab58f82c43c7d87deaaf324d2a9af8f0dd52eb2
Kyle Wood <woodkyle22@gmail.com>
2018-04-06 12:29:42 +0200

    Update : Dashboard bot section limited to 5 bots

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BOTS } from '../bot-testing-data';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  bots = BOTS;
 
   constructor() { }
 
