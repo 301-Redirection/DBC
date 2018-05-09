2eab38da0a1215a66663b51fec48f014936ae038
Byron Antak <bma.byron.antak@gmail.com>
2018-05-09 20:41:43 +0200

    Travis fix x_x

--- heroes/parseHeroData.js
+++ heroes/parseHeroData.js
@@ -1,4 +1,4 @@
-const heroData = require('./heroData.js');
+const heroData = require('./herodata.js');
 /** HELPER FUNCTIONS */
 /** this function removes the first underscore in a string
   *  example: antimage_mana_break -> mana_break
