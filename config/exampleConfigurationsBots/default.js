2876e9732d267adba5853b6572a059e9be326068
russelldutton <rdutton1504@gmail.com>
2018-05-14 17:05:39 +0200

    Added some example object configs and their expected outputs

---
+++ config/exampleConfigurationsBots/default.js
@@ -0,0 +1,201 @@
+module.exports = {
+    body: {
+        configuration: {
+            push: {
+                top: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                mid: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                bot: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+            },
+            farm: {
+                top: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                mid: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                bot: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+            },
+            defend: {
+                top: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                mid: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+                bot: {
+                    compoundConditions: [
+                        {
+                            conditions: [
+                                {
+                                    trigger: 1,
+                                    operator: 1,
+                                    conditional: 5,
+                                    action: 1,
+                                    value: 0.25,
+                                },
+                            ],
+                            logicalOperator: [],
+                        },
+                    ],
+                    initialValue: 0.25,
+                },
+            },
+            roshan: {
+                compoundConditions: [
+                    {
+                        conditions: [
+                            {
+                                trigger: 1,
+                                operator: 1,
+                                conditional: 5,
+                                action: 1,
+                                value: 0.25,
+                            },
+                        ],
+                        logicalOperator: [],
+                    },
+                ],
+                initialValue: 0.25,
+            },
+            roam: {
+                compoundConditions: [
+                    {
+                        conditions: [
+                            {
+                                trigger: 1,
+                                operator: 1,
+                                conditional: 5,
+                                action: 1,
+                                value: 0.25,
+                            },
+                        ],
+                        logicalOperator: [],
+                    },
+                ],
+                initialValue: 0.25,
+            },
+        },
+        name: 'test',
+        description: 'default object test',
+    },
+};
---
+++ config/exampleConfigurationsBots/expectedOutput/default.lua
@@ -0,0 +1,85 @@
+-- test --
+[[ default object test ]]
+
+function UpdateRoshaneDesires()
+    local common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    return common
+end
+
+function UpdateRoamDesires()
+    local common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    return {common, GetTeamMember(((GetTeam() == TEAM_RADIANT) ? TEAM_RADIANT : TEAM_DIRE), RandomInt(1, 5))}
+end
+
+function UpdatePushLaneDesires()
+    local common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local topCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local midCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local botCommon = common
+    
+    return {topCommon, midCommon, botCommon}
+end
+
+function UpdateDefendLaneDesires()
+    local common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local topCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local midCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local botCommon = common
+    
+    return {topCommon, midCommon, botCommon}
+end
+
+function UpdateFarmLaneDesires()
+    local common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local topCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local midCommon = common
+    
+    common = 0.25
+    if (DotaTime() < 5) then
+        common += 0.25
+    end
+    local botCommon = common
+    
+    return {topCommon, midCommon, botCommon}
+end
+
