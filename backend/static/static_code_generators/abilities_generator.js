d71855b1b360f77d234028bc5fabc13b79b9b913
Byron Antak <bma.byron.antak@gmail.com>
2018-07-02 18:49:35 +0200

    Adding static script to dir

---
+++ backend/static/static_code_generators/abilities_generator.js
@@ -0,0 +1,11 @@
+/**
+* Expected input: JSON Object { abilities: "qweqqrewqet", talents: ['l','r','l','l']}
+*
+*   q, w, e, r refer to the ability bound keys t refers to an appropriate talent
+*   l,r refer to left and right for the talent tree
+*/
+
+const hero = drow_ranger;
+const input = {
+    abilities: "qweqqrewqetnqrnt", talents: ['l','r','l','l']
+};
