90f5ca9771d8144455eb2d8f7b991bf3ca2b5d5a
russelldutton <rdutton1504@gmail.com>
2018-07-12 12:07:38 +0200

    Imports for configurator component and pipes

--- frontend/app/bot-config/bot-config.module.ts
+++ frontend/app/bot-config/bot-config.module.ts
@@ -4,7 +4,8 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavbarModule } from '../navbar/navbar.module';
 import { BotConfigComponent } from './bot-config.component';
-import { ConfiguratorComponent, EnumToArrayPipe } from './configurator/configurator.component';
+import { ConfiguratorComponent, EnumToArrayPipe } from './team-desires/configurator/configurator.component';
+import { TeamDesiresComponent, ReversePipe } from './team-desires/team-desires.component';
 
 @NgModule({
     imports: [
@@ -17,6 +18,8 @@ import { ConfiguratorComponent, EnumToArrayPipe } from './configurator/configura
         BotConfigComponent,
         ConfiguratorComponent,
         EnumToArrayPipe,
+        TeamDesiresComponent,
+        ReversePipe,
     ],
     exports: [BotConfigComponent],
 })
