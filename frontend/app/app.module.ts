035f0fc4e7cf46d11ae35e13edba33b77709b013
Stephen Teichert <spteichert@gmail.com>
2018-07-12 22:26:37 +0200

    Drag and Drop feature for Hero Section

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import {SortablejsModule} from 'angular-sortablejs';
 
 // Custom Modules
 import { HomeModule } from './home/home.module';
@@ -21,12 +22,14 @@ import { AuthService } from './auth/auth.service';
 import { AppComponent } from './app.component';
 import { CallbackComponent } from './callback/callback.component';
 import { LoadingComponent } from './core/loading.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         CallbackComponent,
         LoadingComponent,
+        HeroesComponent,
     ],
     imports: [
         BrowserModule,
@@ -39,6 +42,7 @@ import { LoadingComponent } from './core/loading.component';
         NavbarModule,
         BotConfigModule,
         RouterModule,
+        SortablejsModule.forRoot({ animation: 10 }),
     ],
     providers: [
         ApiConnectService,
