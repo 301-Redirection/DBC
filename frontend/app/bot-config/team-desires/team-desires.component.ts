aa5156b81a59573b324aed10ea1b1ce5030b6d67
russelldutton <rdutton1504@gmail.com>
2018-07-12 12:09:59 +0200

    Component for managing config format object. Added dynamic add/remove of configurator component. Added support for multiple lanes for relevant properties. Added support to dynamically added compoundConditions Styles for the component.

--- frontend/app/bot-config/team-desires/team-desires.component.ts
+++ frontend/app/bot-config/team-desires/team-desires.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Pipe, PipeTransform } from "@angular/core";
 import { ConfiguratorComponent } from "./configurator/configurator.component";
-import {
-    ConfigurationFormat,
-    Condition,
-} from "../../ConfigurationFormat";
-import { LowerCasePipe } from "@angular/common";
+import { TeamDesiresService } from '../../services/team-desires.service'
+import { ConfigurationFormat } from '../../ConfigurationFormat'
 
 @Component({
     selector: "app-team-desires",
@@ -12,22 +9,74 @@ import { LowerCasePipe } from "@angular/common";
     styleUrls: ["./team-desires.component.scss"]
 })
 export class TeamDesiresComponent implements OnInit {
-    teamDesires: ConfigurationFormat = new ConfigurationFormat();;
-    constructor() {}
 
-    ngOnInit() {}
+    teamDesires: ConfigurationFormat;
+
+    constructor(private tdService: TeamDesiresService) {}
+
+    ngOnInit() {
+        this.teamDesires = this.tdService.getDefaultConfiguration();
+    }
 
     private trackByFn(index, item) {
         return index;
     }
 
-    private getConditions(prop: string, lane?: string): Condition[] {
+    private getCondGroupIndex(index: number, prop: string, lane: string): number {
         if (lane == undefined) {
-            return this.teamDesires[prop].compoundConditions.conditions;
+            return this.teamDesires[prop].compoundConditions.length - index - 1;
         } else {
             lane = lane.toLowerCase();
-            console.log(this.teamDesires[prop][lane].compoundConditions[0].conditions);
-            return this.teamDesires[prop][lane].compoundConditions[0].conditions;
+            return this.teamDesires[prop][lane].compoundConditions.length - index - 1;
+        }
+    }
+
+    private addCondition(prop: string, condGroup: number, lane?:string): void {
+        condGroup = this.getCondGroupIndex(condGroup, prop, lane);
+        if (lane == undefined) {
+            this.teamDesires[prop].compoundConditions[condGroup].conditions.push(this.tdService.newCondition());
+        } else {
+            lane = lane.toLowerCase();
+            this.teamDesires[prop][lane].compoundConditions[condGroup].conditions.push(this.tdService.newCondition());
+        }
+    }
+
+    private delCondition(prop: string, condGroup: number, index: number, lane?:string) {
+        let ans = window.confirm("Are you sure you wish to delete this Condition?");
+        if (ans) {
+            condGroup = this.getCondGroupIndex(condGroup, prop, lane);
+            if (lane == undefined) {
+                this.teamDesires[prop].compoundConditions[condGroup].conditions.splice(index, 1);
+            } else {
+                lane = lane.toLowerCase();
+                this.teamDesires[prop][lane].compoundConditions[condGroup].conditions.splice(index, 1);
+            }
+        }
+    }
+
+    private addConditionGroup(prop: string, lane?: string): void {
+        if (lane == undefined) {
+            this.teamDesires[prop].compoundConditions.push(this.tdService.newCondGroup());
+        } else {
+            lane = lane.toLowerCase();
+            this.teamDesires[prop][lane].compoundConditions.push(this.tdService.newCondGroup());
+        }
+    }
+
+    private delCondGroup(prop: string, lane?: string) {
+        let ans = window.confirm("Are you sure you wish to delete this Condition Group?");
+        window.alert("OK");
+    }
+}
+
+@Pipe({
+    name: 'reverse',
+    pure: false
+})
+export class ReversePipe implements PipeTransform {
+    transform(values) {
+        if (values) {
+            return values.reverse();
         }
     }
 }
