c930095c561edb7634c593e2cf7bede49a6de752
Kyle Wood <woodkyle22@gmail.com>
2018-07-17 16:59:38 +0200

    Layout fixes

--- frontend/app/items/items.component.ts
+++ frontend/app/items/items.component.ts
@@ -3,150 +3,88 @@ import { SortablejsOptions } from 'angular-sortablejs';
 import { ApiConnectService } from '../services/api-connect.service';
 
 @Component({
-  selector: 'app-items',
-  templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+    selector: 'app-items',
+    templateUrl: './items.component.html',
+    styleUrls: ['./items.component.scss'],
 })
 export class ItemsComponent implements OnInit {
-  // Variables
-  numberOfPools: any;
-  selectedPool: number;
-  selectedPoolArray: any;
+    // Variables
+    selectedItemsArray = [];
 
-  // Temporary test data
-  allHeroes = [
-    {
-      name: 'Axe',
-      class: 'Strength',
-    }, {
-      name: 'Sven',
-      class: 'Strength',
-    }, {
-      name: 'Viper',
-      class: 'Agility',
-    }, {
-      name: 'Sniper',
-      class: 'Agility',
-    }, {
-      name: 'Lina',
-      class: 'Intelligence',
-    }, {
-      name: 'Chen',
-      class: 'Intelligence',
-    },
-  ];
+    // Temporary test data
+    selectedHeroes = [
+        {
+            name: 'Axe',
+            class: 'Strength',
+        },
+        {
+            name: 'Sven',
+            class: 'Strength',
+        },
+        {
+            name: 'Viper',
+            class: 'Agility',
+        },
+        {
+            name: 'Sniper',
+            class: 'Agility',
+        },
+        {
+            name: 'Lina',
+            class: 'Intelligence',
+        },
+        {
+            name: 'Chen',
+            class: 'Intelligence',
+        },
+    ];
 
-  // hero category objects
-  strengthHeroes = [];
-  agilityHeroes = [];
-  intelligenceHeroes = [];
+    basicItems = [
+        {
+            name: 'Town Portal Scroll',
+        },
+        {
+            name: 'Ring of Protection',
+        },
+        {
+            name: 'Iron Branch',
+        },
+    ];
 
-  pool1 = [];
-  pool2 = [];
-  pool3 = [];
-  pool4 = [];
-  pool5 = [];
+    optionsSource: SortablejsOptions = {
+        group: {
+            name: 'clone-group',
+            pull: 'clone',
+            put: false,
+        },
+    };
 
-  optionsSource: SortablejsOptions = {
-    group: {
-      name: 'clone-group',
-      pull: 'clone',
-      put: false,
-    },
-  };
+    optionsTarget: SortablejsOptions = {
+        group: 'clone-group',
+    };
 
-  optionsTarget: SortablejsOptions = {
-    group: 'clone-group',
-  };
+    constructor(private api: ApiConnectService) {}
 
-  constructor(private api: ApiConnectService) { }
-
-  ngOnInit() {
-    this.numberOfPools = [1, 2, 3, 4, 5];
-    this.selectedPool = 1;
-    this.selectedPoolArray = this.pool1;
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    // database call to retrieve all dota heroes
-    // this.api.getAllHeroes().subscribe((data) => {
-    //     this.allHeroes = data;
-    //     this.sortHeroData();
-    // });
-
-    this.sortHeroData();
-  }
-
-  sortHeroData(): void {
-    this.allHeroes.forEach((hero) => {
-      if (hero.class === 'Strength') {
-        this.strengthHeroes.push(hero);
-      } else if (hero.class === 'Agility') {
-        this.agilityHeroes.push(hero);
-      } else if (hero.class === 'Intelligence') {
-        this.intelligenceHeroes.push(hero);
-      }
-    });
-  }
-
-  setSelectedPool(pool: number): void {
-    this.selectedPool = pool;
-    switch (pool) {
-      case 1:
-        this.selectedPoolArray = this.pool1;
-        break;
-      case 2:
-        this.selectedPoolArray = this.pool2;
-        break;
-      case 3:
-        this.selectedPoolArray = this.pool3;
-        break;
-      case 4:
-        this.selectedPoolArray = this.pool4;
-        break;
-      case 5:
-        this.selectedPoolArray = this.pool5;
-        break;
+    ngOnInit() {
+        this.getItems();
     }
-  }
 
-  togglePools(): void {
-    this.pool1 = [];
-    this.pool2 = [];
-    this.pool3 = [];
-    this.pool4 = [];
-    this.pool5 = [];
-    this.selectedPool = 1;
-    this.selectedPoolArray = this.pool1;
+    getItems(): void {
+        // database call to retrieve all dota heroes
+        // this.api.getAllHeroes().subscribe((data) => {
+        //     this.allHeroes = data;
+        //     this.sortHeroData();
+        // });
 
-    if (this.numberOfPools.length > 1) {
-      this.numberOfPools = [1];
-      document.getElementById('poolTabs').style.height = '0';
-      document.getElementById('poolTabs').style.visibility = 'hidden';
-    } else {
-      this.numberOfPools = [1, 2, 3, 4, 5];
-      document.getElementById('poolTabs').style.height = '42px';
-      document.getElementById('poolTabs').style.visibility = 'visible';
+        this.sortItemData();
     }
-  }
 
-  removeHero(hero, pool): void {
-    const index = pool.indexOf(hero);
-    if (index !== -1) {
-      pool.splice(index, 1);
+    sortItemData(): void {
+        // TODO
+        // Sort items
     }
-    document.getElementById(`poolLink${this.selectedPool - 1}`).click();
-  }
 
-  resetPools(): void {
-    this.pool1 = [];
-    this.pool2 = [];
-    this.pool3 = [];
-    this.pool4 = [];
-    this.pool5 = [];
-    this.selectedPool = 1;
-    this.selectedPoolArray = this.pool1;
-  }
-
+    reset() : void {
+        this.selectedItemsArray = [];
+    }
 }
