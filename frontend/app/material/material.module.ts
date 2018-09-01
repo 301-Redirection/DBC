e85a7fb3c86010a16c8f2521502a9b16cc60b257
russelldutton <rdutton1504@gmail.com>
2018-09-01 20:42:17 +0200

    New material modules to import

--- frontend/app/material/material.module.ts
+++ frontend/app/material/material.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-    imports: [MatSliderModule],
-    exports: [MatSliderModule],
+    imports: [
+        MatSliderModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatTooltipModule,
+    ],
+    exports: [
+        MatSliderModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatTooltipModule,
+    ],
 })
 export class MaterialModule {}
