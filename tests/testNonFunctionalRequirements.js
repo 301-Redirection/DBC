3491709e879c50a595176b3d8026fe5a6165fa37
Jeffrey Russell <jeffreymrussell@gmail.com>
2018-09-20 21:05:57 +0200

    Moved diagnostics test to actual test

--- tests/testNonFunctionalRequirements.js
+++ tests/testNonFunctionalRequirements.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-console */
 /**
  *  This file is used to time how long it takes to generate the bot scripts from
  *  the example objects to
  *
  */
 const path = require('path');
-
-process.env.NODE_PATH = path.join(__dirname, '..', 'backend');
+process.env.NODE_PATH = path.join(__dirname, '../backend');
+require('module').Module._initPaths();
 const exampleObjectDefault = require('../config/exampleConfigurationsBots/default.js');
 const exampleObjectDefaultAllHeroes = require('../config/exampleConfigurationsBots/defaultAllHeroes.js');
 const exampleObjectDefaultHeroesByPos = require('../config/exampleConfigurationsBots/defaultHeroesByPosition.js');
@@ -21,33 +20,37 @@ const id = 'timing';
 const botId = 'test';
 
 let start = process.hrtime();
-function elapsedTime(statement) {
+
+
+function elapsedTime() {
     // 3 decimal places
     const precision = 3;
     // divide by a million to get nano seconds to milliseconds
     const elapsed = process.hrtime(start)[1] / 1000000;
-    const startTime = process.hrtime(start)[0];
     const fixedPrecisionTimeElapsed = elapsed.toFixed(precision);
-    console.log(`${startTime} s, ${fixedPrecisionTimeElapsed} ms - ${statement}`);
+    // console.log(`${startTime} s, ${fixedPrecisionTimeElapsed} ms - ${statement}`);
     start = process.hrtime(); // reset the timer
+    return fixedPrecisionTimeElapsed;
 }
+describe('Non-functional requirements testing', () => {
+    // creating a bot in a valid way
+    it('-- time for each must be under 10 seconds', (done) => {
+        const objectsToTime = [
+            exampleObjectDefault,
+            exampleObjectDefaultAllHeroes,
+            exampleObjectDefaultHeroesByPos,
+            exampleObjectDefaultItemsSpecified,
+            exampleObjectDefaultAbilitiesSpecified,
+            exampleObjectComplexOne,
+        ];
+        // writeScripts(objectsToTime[0], response, id, `timing_${botId}`);
+        objectsToTime.forEach((object) => {
+            start = process.hrtime();
+            writeScripts(object, response, id, `${botId}`);
+            const time = elapsedTime();
+            expect(time).toBeLessThan(3000);
+        });
 
-const objectsToTime = {
-    exampleObjectDefault,
-    exampleObjectDefaultAllHeroes,
-    exampleObjectDefaultHeroesByPos,
-    exampleObjectDefaultItemsSpecified,
-    exampleObjectDefaultAbilitiesSpecified,
-    exampleObjectComplexOne,
-};
-start = process.hrtime();
-if (objectsToTime) {
-    console.log(Object.entries(objectsToTime));
-    Object.entries(objectsToTime).forEach((object) => {
-        const propName = object[0];
-        const value = object[1];
-        writeScripts(value, response, id, `${botId}_${propName}`);
-        elapsedTime(`writeScripts -> ${propName}`);
+        done();
     });
-}
-
+});
