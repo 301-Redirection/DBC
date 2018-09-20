910d96d9eb4f7d8a5f63738fdb4d8a4e7071695f
Stephen Teichert <spteichert@gmail.com>
2018-09-20 13:22:14 +0200

    Fixed resetting of hero pool.

--- frontend/app/services/bot-config-data.service.ts
+++ frontend/app/services/bot-config-data.service.ts
@@ -128,9 +128,20 @@ export class BotConfigDataService {
     // Saves the currently selected heroes to the config
     public updateSelectedHeroes(heroes: any): void {
         this.selectedHeroes.next(heroes);
+        console.log('Selected heroes:', heroes);
         heroes.forEach((hero) => {
             if (hero !== undefined) {
                 this.addHeroSpecification(hero['programName']);
+            }
+        });
+    }
+
+    public clearSelectedHeroes(heroes: any): void {
+        this.selectedHeroes.next(heroes);
+        console.log('Selected heroes:', heroes);
+        heroes.forEach((hero) => {
+            if (hero !== undefined) {
+                this.removeHeroSpecification(hero['programName']);
             }
         });
     }
