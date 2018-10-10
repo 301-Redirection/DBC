b58b81b817edf7919ff558d901d4ba8b7cb4a449
Stephen Teichert <spteichert@gmail.com>
2018-10-11 00:08:09 +0200

    Home page modifications

--- frontend/app/home/home.component.ts
+++ frontend/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component(
     {
@@ -11,11 +13,23 @@ import { Title } from '@angular/platform-browser';
 export class HomeComponent implements OnInit {
     pageTitle = 'Dota 2 Bot Scripting - Home';
 
-    constructor(private title: Title) {
+    constructor(
+        private title: Title, 
+        private router: Router,
+        private auth: AuthService,
+    ) {
         this.title.setTitle(this.pageTitle);
     }
 
     ngOnInit() { }
+
+    navigate(route) {
+        if (this.auth.loggedIn) {
+            this.router.navigate([route]);
+        } else {
+            this.auth.login();
+        }
+    }
 }
 
 export default 'HomeComponent';
--- frontend/app/home/home.module.ts
+++ frontend/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule,
     ],
     declarations: [
         HomeComponent,
