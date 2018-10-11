b35b3836cbd5cdf9fce7fc739b8404a59a7f8f00
Jeffrey Russell <jeffreymrussell@gmail.com>
2018-10-11 22:25:18 +0200

    making it compile

--- frontend/app/bot-config/bot-config.component.ts
+++ frontend/app/bot-config/bot-config.component.ts
@@ -12,7 +12,7 @@ import * as globalConfig from '../../../config/config.js';
 import { TeamDesiresComponent } from './team-desires/team-desires.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { AbilitiesComponent } from './abilities/abilities.component';
-import { ItemsComponent } from './items/items.component';
+// import { ItemsComponent } from './items/items.component';
 import { BotConfigDataService } from '../services/bot-config-data.service';
 
 declare var swal: any;
@@ -138,7 +138,7 @@ export class BotConfigComponent implements OnInit, AfterViewInit {
         this.teamDesiresComponent.reset();
         this.heroesComponent.reset();
         this.abilitiesComponent.reset();
-        this.itemsComponent.reset();
+        // this.itemsComponent.reset();
         this.botConfigData.reset();
     }
 
