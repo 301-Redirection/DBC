5f35d7a7507314423240f4832c0b22967f00f98f
Kyle Wood <woodkyle22@gmail.com>
2018-10-12 15:28:18 +0200

    WIP - update home page

--- frontend/app/home/home.component.ts
+++ frontend/app/home/home.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from '../auth/auth.service';
 )
 export class HomeComponent implements OnInit {
     pageTitle = 'Dota 2 Bot Configurator - Home';
+    dotaLogoURL = '../../assets/images/dota_logo.png';
 
     constructor(
         private title: Title,
